115a896db827c9a966809c1e33f64c755331fdff
Mark Pashmfouroush <mark@markpash.me>
2023-05-22 18:41:45 +0100

    update flake.lock
    Signed-off-by: Mark Pashmfouroush <mark@markpash.me>

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "hashinixpkgs";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
+  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
   inputs.flake-parts.url = "github:hercules-ci/flake-parts";
 
   outputs = inputs@{ flake-parts, ... }: flake-parts.lib.mkFlake { inherit inputs; } {
