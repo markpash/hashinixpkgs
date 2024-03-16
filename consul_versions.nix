2e79d2e044f2b14d2e10d85da76e4dcc7d974c29
Mark Pashmfouroush <mark@markpash.me>
2024-03-16 11:05:38 +0000

    disable consul_1_12_*
    Signed-off-by: Mark Pashmfouroush <mark@markpash.me>

--- consul_versions.nix
+++ consul_versions.nix
@@ -53,15 +53,4 @@ lib.attrsets.mapAttrs'
   "1.13.2" = { sha256 = "sha256-+5I5hsVuLZve4FJHe41rKycWuKNv7UdxCSi4kaKk2/w="; vendorSha256 = "sha256-SjTG1/WhfGhLuUherVHEC3PmDz4HLMS2Cg0ReKTm0zk="; };
   "1.13.1" = { sha256 = "sha256-Svclb+tLb0gk3xhoslVvAZZf0LR2M5nR9BmMYLBBHck="; vendorSha256 = "sha256-bPfS8hoH45ad34CsR+9WLEDCDsx1E/ZKdipVZqirfDY="; };
   "1.13.0" = { sha256 = "sha256-bPLVQ5XvOk6cWtjHNtvZ3McM2SKVbjY6280rIldInpQ="; vendorSha256 = "sha256-bPfS8hoH45ad34CsR+9WLEDCDsx1E/ZKdipVZqirfDY="; };
-
-  "1.12.9" = { sha256 = "sha256-OEgSqa7+sZGHzx5PiG5otLH1p6yo5CMhEssisYt9cKQ="; vendorSha256 = "sha256-P1GYqJdPt0esd6oacAh8w7Pvo0cKaQ1zFy8UMCENpTw="; };
-  "1.12.8" = { sha256 = "sha256-aOA+Hw3hcpLt8qiNfZDFP+i3sLw4jF89Yw+1vMkhjqI="; vendorSha256 = "sha256-kLXxNhyOS36RlHtvOnOoLQLv8qfoCZD2XXepkDpBqfM="; };
-  "1.12.7" = { sha256 = "sha256-I14sTX9ISMMVHmveDlfN90GNk81b8VY1w1ZBqyq14N0="; vendorSha256 = "sha256-8Es4V5kTi/FOv2cY4whtzfUkliuRCjWVKy+K3J987H4="; };
-  "1.12.6" = { sha256 = "sha256-ap4yLR0k+mtOijOolVfhuimCRENB5IxQNHxA0h7jgm0="; vendorSha256 = "sha256-p7YJwY87TdtUdd6GvWe2ORNw4lyz24ST5Q5EcDJlFig="; };
-  "1.12.5" = { sha256 = "sha256-mAkYaxxXLQevBsmPHMSrVYE4wEew26q292zokR+SINo="; vendorSha256 = "sha256-8+6zGvKbnGtcZ1ageSenBUnqAdZiw9+5E9JzHGnyqOY="; };
-  "1.12.4" = { sha256 = "sha256-bRNPPIF1WC0tTiACo/YgL99nDdh4sWag0X+Rczjyxds="; vendorSha256 = "sha256-8PmjHAr9QLkeEpc5/+1SbtgEwijCEp2BbOBApSa9eYM="; };
-  "1.12.3" = { sha256 = "sha256-QCSlRGbYCOOS81M7sl8Skr1/DhEX+U+0dCroWydzifw="; vendorSha256 = "sha256-3WjmAbY7AVApZZXdCfbjr2X9I8vvwR2EfNmCJv9Gk6A="; };
-  "1.12.2" = { sha256 = "sha256-X2vW6fnT5IY+YxGnAP5Flk610vIwLg3Qu/rEerFyNCw="; vendorSha256 = "sha256-Qcm+uPlvzg0r+a/rYVCUaQ7iIgCpW7MyL7KrHkNm4XQ="; };
-  "1.12.1" = { sha256 = "sha256-VSxgtsCPi3EdRWon3VBE9sC0lr5k5uDn/iMqrKAJeM0="; vendorSha256 = "sha256-Qcm+uPlvzg0r+a/rYVCUaQ7iIgCpW7MyL7KrHkNm4XQ="; };
-  "1.12.0" = { sha256 = "sha256-O47s9eNZsqg0zpgoK2S4JvuOEqqVWvx8YmbQTvCw0Co="; vendorSha256 = "sha256-NRFGt8YA179C/OvyMGXYaZMUH685pJfdGB0AhMrCixs="; };
 }
