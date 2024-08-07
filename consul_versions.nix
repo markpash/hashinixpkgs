849daf91d4ab0683e34e12afa4fef393b529a8b8
Mark Pashmfouroush <mark@markpash.me>
2024-08-07 07:53:55 +0100

    add consul_1_18_2
    Signed-off-by: Mark Pashmfouroush <mark@markpash.me>

--- consul_versions.nix
+++ consul_versions.nix
@@ -8,6 +8,7 @@ lib.attrsets.mapAttrs'
     )
   )
 {
+  "1.18.2" = { sha256 = "sha256-9feeWsCAZKip+AYUJTCcqFOTfxsUFMzTRqHjo/Pptho="; vendorHash = "sha256-bBc3qgPUOmR8c/sFyiskePKLKjHTQcWVTLY6esFIRTc="; };
   "1.18.1" = { sha256 = "sha256-r1xdz1rjvbvB93hRpvTNQwSqQLOJwqMhqCiXdIttY10="; vendorHash = "sha256-DcpEHJ88Ehz5m+ddMd44mYTz0agwYhoels5jWJzu1EM="; };
   "1.18.0" = { sha256 = "sha256-Xhh6Rrcv/FoBjzhWR59gQ/R4A3ynqWYS8djNe3CnGCE="; vendorHash = "sha256-pNFjLXjtgsK8fjCCmjYclZw1GM4BfyzkTuaRCRIMJ3c="; };
 
